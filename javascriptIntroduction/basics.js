d8006d5a054f931bbc4fc5b00f66772613212329
Aravindhkollabathula <125636628+Aravindhkollabathula@users.noreply.github.com>
2023-03-11 09:59:35 +0530

    Add files via upload

---
+++ javascriptIntroduction/basics.js
@@ -0,0 +1,81 @@
+document.getElementById("title").innerHTML = "learning Html,Css And JavaScript"
+document.getElementById("title").style.color = "blue"
+document.getElementById("title").style.backgroundColor = "yellow"
+
+document.getElementById("content").style.fontSize = "20px"
+
+console.log("aravindh")
+console.log(20+30)
+
+
+ alert("hello JavaScript")
+
+ var feeling = window.prompt("Are You Happy")
+ console.log(feeling)
+ 
+ 
+
+ document.getElementById("content").innerHTML = feeling
+window.confirm("aravindh")
+
+document.write("aravindh")
+
+window.print()
+
+datatypes
+
+
+let laptopname = window.prompt("what is your laptop name")
+console.log(laptopname)
+document.getElementById("laptop").innerHTML = laptopname
+
+var price = 55000
+document.getElementById("cost").innerHTML = price
+
+var cost = window.prompt("enter the cost")
+console.log(typeof cost)
+document.getElementById("cost").innerHTML = cost
+
+var a = window.prompt("enter value of a")
+var b = window.prompt("enter value of b")
+var c = a+b;
+console.log(c)
+console.log(typeof c)
+
+var a = 10
+var b = 20
+var c = a+b
+console.log(c)
+console.log(typeof a,typeof b,typeof c)
+
+var a = "kollabathula"
+var b = "aravindh"
+var c = a+b
+console.log(c)
+console.log(typeof a,typeof b,typeof c)
+
+var day = "saturday"
+console.log("today is",day)
+console.log(typeof day)
+
+var tenapples = "fifty"
+var oneapplecost = 10
+var tenapplescost = tenapples * 3
+console.log(tenapplescost)
+console.log(typeof tenapplescost)
+
+var courseName = "frontend"
+var duration = "2 months"
+var IMPORTANT_PROJECTS = "WEB PAGES"
+var project1 = "simple form"
+var $total_projects = 10
+var _tools = ["vscode","git","github"]
+var syllabus = "topic1"
+var course = "iam learning" + " " + courseName + " " + "course" + ",the duration of course is" + " " + duration
+console.log(course)  
+
+var totalprice;
+console.log(totalprice)
+
+totalprice = totalprice + 100
+console.log(totalprice)
